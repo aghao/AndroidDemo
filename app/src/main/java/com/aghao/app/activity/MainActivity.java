dd92c1f241c8ce3e2574d890501540911fbc1338
aghao <yinhao158@163.com>
2019-09-15 02:49:16 +0800

    测试scrollTo 无关紧要

--- app/src/main/java/com/aghao/app/activity/MainActivity.java
+++ app/src/main/java/com/aghao/app/activity/MainActivity.java
@@ -31,7 +31,8 @@ public class MainActivity extends AppCompatActivity {
         btnViewTouch.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                Intent intent = new Intent(MainActivity.this, ViewTouchActivity.class);
+                startActivity(intent);
             }
         });
     }
--- app/src/main/java/com/aghao/app/activity/ScrollActivity.java
+++ app/src/main/java/com/aghao/app/activity/ScrollActivity.java
@@ -2,17 +2,22 @@ package com.aghao.app.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.Scroller;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.aghao.app.R;
 
 public class ScrollActivity extends AppCompatActivity {
 
+    private static final String TAG = "justyin";
+
+    TextView tvCord;
     Scroller myScroller;
     GestureDetector myGestureDetector;
     ImageView imgBoy;
@@ -22,46 +27,26 @@ public class ScrollActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.scroll_main);
 
-        imgBoy = (ImageView)findViewById(R.id.imgboy);
+        tvCord = findViewById(R.id.tvCord);
+        imgBoy = findViewById(R.id.imgboy);
         imgBoy.setOnTouchListener(imgOnTouchListener);
-        myGestureDetector = new GestureDetector(this, new GestureDetector.OnGestureListener() {
-            @Override
-            public boolean onDown(MotionEvent e) {
-                return true;
-            }
-
-            @Override
-            public void onShowPress(MotionEvent e) {
-
-            }
-
-            @Override
-            public boolean onSingleTapUp(MotionEvent e) {
-                return false;
-            }
-
-            @Override
-            public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
-                Toast.makeText(ScrollActivity.this, "你在滑动手指", Toast.LENGTH_SHORT).show();
-                return false;
-            }
-
-            @Override
-            public void onLongPress(MotionEvent e) {
-
-            }
-
-            @Override
-            public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
-                return false;
-            }
-        });
     }
 
     View.OnTouchListener imgOnTouchListener = new View.OnTouchListener() {
         @Override
         public boolean onTouch(View v, MotionEvent event) {
-            return myGestureDetector.onTouchEvent(event);
+            switch (event.getAction()) {
+                case MotionEvent.ACTION_UP:
+                    v.performClick();
+                    break;
+                case MotionEvent.ACTION_DOWN:
+                    Log.i(TAG, "ACTION_DOWN");
+                    break;
+                case MotionEvent.ACTION_MOVE:
+                    tvCord.setText("x坐标："+event.getX()+"|"+"y坐标："+event.getY());
+                    break;
+            }
+            return true;
         }
     };
 
